a03b673c983500f375d693adf8875c6c02911676
farhanfadhilah <farhanfadhilah87@gmail.com>
2024-02-17 22:02:56 +0700

    Initial Commit

---
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+struct CalculatorBrain {
+
+    var bmi: BMI?
+    
+    
+    mutating func calculateBMI(height: Float, weight: Float) {
+        let bmiValue = weight / (height * height)
+        
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: UIColor.blue)
+        } else if bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, advice: "Fit as fiddle", color: UIColor.green)
+        } else {
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: UIColor.red)
+        }
+                
+       
+    }
+    
+    func getBMIValue() -> String {
+        return String(format: "%.1f", bmi?.value ?? 0.0)
+    }
+    
+    func getAdvice() -> String {
+        return bmi?.advice ?? "No advice"
+    }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        
+    }
+    
+    
+}
